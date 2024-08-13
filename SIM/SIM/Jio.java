d185b205385d3855b51766db0dbbb97ff806beed
Praveen Kumar <109404771+praveen1814@users.noreply.github.com>
2024-08-13 18:48:53 +0530

    Ioc Container

---
+++ SIM/SIM/Jio.java
@@ -0,0 +1,12 @@
+package Spring_Core.SIM;
+
+public class Jio implements Sim{
+		public void calling()
+		{
+			System.out.println("calling from jio sim");
+		}
+		public void data()
+		{
+			System.out.println("browsing using jio sim");
+		}
+}
